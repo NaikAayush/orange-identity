57f484376ba118bafd50c657e222ace2f8bd061d
NaikAayush <ayush@pesu.pes.edu>
2021-09-28 02:16:07 +0530

    added web3 imports and setup

--- client/src/app/services/web3/web3.service.ts
+++ client/src/app/services/web3/web3.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import Web3 from 'web3';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Web3Service {
+  private web3: Web3;
 
-  constructor() { }
+  constructor() {
+    this.web3 = new Web3('web3-provider-url');
+  }
 }
